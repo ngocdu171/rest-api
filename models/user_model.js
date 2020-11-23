647f38566d928400c1b810afae5b7bd5e09fe04a
Du Pham <ngocdu171@gmail.com>
2020-11-23 22:33:08 +0200

    complete restApi user-table

--- models/user_model.js
+++ models/user_model.js
@@ -1,4 +1,7 @@
 var db = require('../database');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 var user = {
     get: function(callback) {
         return db.query('select * from user_table order by id_user desc', callback);
@@ -7,15 +10,19 @@ var user = {
         return db.query('select * from user_table where id_user=$1', [id], callback);
     },
     add: function(user, callback) {
-        return db.query('insert into user_table(username,password) values($1,$2)',
-        [user.username,user.password], callback);
+        bcrypt.hash(user.password, saltRounds, function(err, hash) {
+            return db.query('insert into user_table(username,password) values($1,$2)',
+            [user.username,hash], callback);
+        });
     },
     delete: function(id, callback) {
         return db.query('delete from user_table where id_user', [id], callback);
     },
     update: function(id, user, callback) {
-        return db.query('update user_table set username=$1, password=$2 where id_user=$3',
-        [user.username,user.password, id], callback);
+        bcrypt.hash(user.password, saltRounds, function(err, hash) {
+            return db.query('update user_table set username=$1, password=$2 where id_user=$3',
+        [user.username,hash, id], callback);
+        });
     },
     searchByName: function(value, callback) {
         var nameLike="%"+value+"%";
